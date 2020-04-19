29a2ba16aee964be8ac38d678aeb300dcec9c468
korrio <iamnotkorr@gmail.com>
2020-04-19 17:52:48 +0700

    fix indent

--- firebase-setup/config.js
+++ firebase-setup/config.js
@@ -1,15 +1,15 @@
 export default {
-	// apiKey: process.env.FB_API_KEY,
-	// authDomain: process.env.FB_AUTH_DOMAIN,
-	// databaseURL: process.env.FB_DATABASE_URL,
-	// projectId: process.env.FB_PROJECT_ID,
-	// storageBucket: process.env.FB_STORAGE_BUCKET,
-	// messagingSenderId: process.env.FB_MESSAGE_SENDER_ID
-	apiKey: 'AIzaSyBDT7swAkdQkyTDnD8tO-ryUSwftiSf3es',
-	authDomain: 'botbaht.firebaseapp.com',
-	databaseURL: 'https://botbaht.firebaseio.com',
-	projectId: 'botbaht',
-	storageBucket: 'botbaht.appspot.com',
-	messagingSenderId: '621191134813',
-	appId: '1:621191134813:web:7d48026802ffbdb1ab57b4'
-};
+  // apiKey: process.env.FB_API_KEY,
+  // authDomain: process.env.FB_AUTH_DOMAIN,
+  // databaseURL: process.env.FB_DATABASE_URL,
+  // projectId: process.env.FB_PROJECT_ID,
+  // storageBucket: process.env.FB_STORAGE_BUCKET,
+  // messagingSenderId: process.env.FB_MESSAGE_SENDER_ID
+  apiKey: 'AIzaSyBDT7swAkdQkyTDnD8tO-ryUSwftiSf3es',
+  authDomain: 'botbaht.firebaseapp.com',
+  databaseURL: 'https://botbaht.firebaseio.com',
+  projectId: 'botbaht',
+  storageBucket: 'botbaht.appspot.com',
+  messagingSenderId: '621191134813',
+  appId: '1:621191134813:web:7d48026802ffbdb1ab57b4'
+}
